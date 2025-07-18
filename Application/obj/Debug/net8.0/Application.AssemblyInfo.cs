7bf65e887fdb2d8f54f4dc68fb48b96b46a78e09
Thirumalaiah1021 <tirumalaiah.shiriveri@gmail.com>
2025-07-19 00:29:20 +0530

    Secnd Commit

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99d2b664651021e866cd79dadeb2ae9a4f7bd68a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
